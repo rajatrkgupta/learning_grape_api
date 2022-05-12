c51ad30b4b03944d949cc35656d396c9b051f31e
Rajat <rajatrkgupta@gmail.com>
2022-05-12 11:22:50 +0530

    grape routes mounting on rails 7.0

---
+++ app/api/book_store/v1/books.rb
@@ -0,0 +1,30 @@
+module BookStore
+  module V1
+    class Books < Grape::API
+      version 'v1', using: :path
+      format :json
+      prefix :api
+      resource :books do
+        desc 'Return list of books'
+        get do
+          books = Book.all
+          present books
+        end
+      end
+    end
+  end
+end
+
+
+# class BookStore::V1::Books < Grape::API
+#       version 'v1', using: :path
+#       format :json
+#       prefix :api
+#       resource :books do
+#         desc 'Return list of books'
+#         get do
+#           books = Book.all
+#           present books
+#         end
+#       end
+#     end
