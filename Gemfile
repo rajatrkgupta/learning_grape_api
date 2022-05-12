15865db3b641b4cec9d6a6cbc17127268a301fc9
Rajat <rajatrkgupta@gmail.com>
2022-05-12 11:19:16 +0530

    add grape and other gems with seeds some records

--- Gemfile
+++ Gemfile
@@ -48,6 +48,11 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem "grape", "~> 1.6"
+gem "grape-entity", "~> 0.10.1"
+gem "grape_on_rails_routes", "~> 0.3.2"
+gem "faker", "~> 2.20"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+15.times do
+  Book.create!(isbn: Faker::Number.number(digits: 4),
+               title: Faker::Superhero.power,
+               stock: Faker::Number.between(from: 2, to: 9))
+end
+
+book_ids = Book.ids
+95.times do
+  Flow.create!(book_id: book_ids.sample,
+               new_stock: Faker::Number.between(from: 2, to: 15),
+               previous_stock: Faker::Number.between(from: 2, to: 15))
+end
